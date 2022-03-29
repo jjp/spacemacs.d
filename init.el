5e45bf8e1f026e0223d9f5c0e5d762420ae4e418
Jonathan Prettyman <jonathanprettyman@Jons-Computer.local>
2022-03-29 07:27:49 -0700

    Fixed undo-tree file target directory

--- init.el
+++ init.el
@@ -332,5 +332,7 @@ you should place your code here."
   ;;   :config
   ;;   (global-set-key [remap kill-ring-save] #'easy-kill)
   ;;   (global-set-key [remap mark-sexp] #'easy-mark))
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+
   )
 
